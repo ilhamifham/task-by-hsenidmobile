8a13665807e4c2aad5f9f104c9315df6de31a274
Ilham Ifham <ilhaamifham@gmail.com>
2025-07-20 13:17:33 +0530

    fixed navbar issue

--- src/views/APIDocsView.vue
+++ src/views/APIDocsView.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="px-4 py-12">
         <div class="max-w-md mx-auto md:max-w-[46rem] xl:max-w-[65.75rem]">
-            <h3 class="text-4xl font-bold -mt-1.75">GET/v1/users</h3>
+            <h3 class="text-4xl font-bold -mt-1.75 text-black">GET/v1/users</h3>
             <p class="mt-1 mb-4.5">Retrieves a list of all users associated with your project.</p>
-            <h4 class="text-xl font-bold mb-2.25">Example Request</h4>
+            <h4 class="text-xl font-bold mb-2.25 text-black">Example Request</h4>
             <div class="bg-neutral-100 p-4 overflow-x-auto rounded-2xl mb-6.25">
                 <code>
                             <pre>fetch('https://api.apipro.com/v1/users', {</pre>
@@ -16,7 +16,7 @@
                             <pre>.then(data => console.log(data));</pre>
                         </code>
             </div>
-            <h4 class="text-xl font-bold mb-2.25">Example Response (200 OK)</h4>
+            <h4 class="text-xl font-bold mb-2.25 text-black">Example Response (200 OK)</h4>
             <div class="bg-neutral-100 p-4 overflow-x-auto rounded-2xl">
                 <code>
                             <pre>{</pre>
